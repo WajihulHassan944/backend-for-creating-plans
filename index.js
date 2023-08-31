88706fb1a373cda5994e7b2940ed115c72ab32da
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-08-31 08:26:07 +0500

    update 1

--- index.js
+++ index.js
@@ -470,15 +470,6 @@ io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
 });
-socket.on('disconnect', () => {
-  const index = players.indexOf(socket.id);
-  if (index !== -1) {
-    players.splice(index, 1);
-    io.emit('playerLeft', socket.id);
-  }
-  console.log('A user disconnected:', socket.id);
-});
-
   
   app.get("/", (req,res) =>{
       res.send("hello");
